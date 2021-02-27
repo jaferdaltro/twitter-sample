a33a33957de5d777caac9bc0bee5c313ff5625db
Jafer Daltro <jaferjr@gmail.com>
2021-02-27 06:33:57 -0300

    Create helper full_title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base = "Ruby on Rails"
+    @base = "Ruby on Rails Sample App"
   end
 
   test "should get root" do
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base}"       
+    assert_select "title", @base       
   end
 
   test "should get help" do
