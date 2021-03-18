5740ff7ebfc32959f5100468880140e8acfe4f06
Jafer Daltro <jaferjr@gmail.com>
2021-03-18 10:38:53 -0300

    Finish advanced login

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = User.first
+  end
+  
+
   test "should get new" do
     get login_path
     assert_response :success
   end
+
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -75,13 +75,33 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
     assert_not is_logged_in?
+    #try to logout in a second window
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
-    assert_select "a[href=?]", login_path, count: 1
+    assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
     
   
+  end
+
+  test "authenticate? should return false for a user with nil digest"do
+    assert_not @user.authenticated?('')
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    # assert_not cookies[:remember_token].blank?
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    #login to set the cookie
+    log_in_as(@user, remember_me: '1')
+    #login again to see if the cookie is deleted
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 
  
