91ce8799bbd266ed0b4198a1d048b95b151bffe8
Jafer Daltro <jaferjr@gmail.com>
2021-03-23 09:16:51 -0300

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,23 @@
 class SessionsController < ApplicationController
   def new
+    
   end
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]  
-      reset_session # security issues
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      log_in @user
-      redirect_to forwarding_url || @user
+      if @user.activated?
+        forwarding_url = session[:forwarding_url]  
+        reset_session # security issues
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        log_in @user
+        redirect_to forwarding_url || @user
+      else
+        message = "Account not activated"
+        message += "Check your email for the activation link"
+        flash[:warning] = message
+        redirect_to root_url
+      end
       # debugger
     else
       flash.now[:danger] = "invalid email/password combination "
