51553933a39917fe7d2ea3642ec26d9263ca01ab
Jafer Daltro <jaferjr@gmail.com>
2021-03-27 13:11:23 -0300

    Fish section 'Showing microposts'

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,20 @@ User.create!(name:  "User Example",
 
 # Generate a bunch of additional users.
 99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-    email: email,
-    password:              password,
-    password_confirmation: password,
-    activated: true,
-    activated_at: Time.zone.now)
+  name  = Faker::Name.name
+  # email = "example-#{n+1}@railstutorial.org"
+  email = Faker::Internet.unique.email
+  password = "password"
+  User.create!(name:  name,
+      email: email,
+      password:              password,
+      password_confirmation: password,
+      activated: true,
+      activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(7)
+55.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each  {|user| user.microposts.create!(content: content)}
 end
